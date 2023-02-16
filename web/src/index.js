f9d3c5c004b39c323b343017cef473410ed74b90
hexcyan <xcyan.li@gmail.com>
2023-02-16 14:40:00 +1100

    more desn

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './components/navbar';
+import App from './app';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
+        <App class=""/>
   </React.StrictMode>
 );
 
