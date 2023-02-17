0bd5f8414b318289404e527a973f3aeff5301419
hexcyan <xcyan.li@gmail.com>
2023-02-17 20:54:06 +1100

    oopsies

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,19 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: [
-        './src/components/*.{js,jsx,ts,tsx}',
-        './src/index.js'
-    ],
-    theme: {
-        colors: {
-          'cyan': '#00FFFF',
-          'rose': '#FF546E',
-          'orang': '#FF9F38',
-          'dark': '#121233',
-          'white': "#FFFFFF",
-          'black': "#000000"
-        }
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/index.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'cyan': {
+          '50': '#edfffe',
+          '100': '#c0feff',
+          '200': '#81fbff',
+          '300': '#3af8ff',
+          '400': '#00ffff',
+          '500': '#00e1e2',
+          '600': '#00b2b7',
+          '700': '#008c91',
+          '800': '#006c72',
+          '900': '#04585d',
+        },      
+        'rose': "#FF3053",
+        'dark': '#10102E'
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
