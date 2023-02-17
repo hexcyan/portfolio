bf2479a8fa98a0d5b9faa9481facd66b99bdb3d2
hexcyan <xcyan.li@gmail.com>
2023-02-17 23:48:12 +1100

    cymk

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -8,20 +8,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'cyan': {
-          '50': '#edfffe',
-          '100': '#c0feff',
-          '200': '#81fbff',
-          '300': '#3af8ff',
-          '400': '#00ffff',
-          '500': '#00e1e2',
-          '600': '#00b2b7',
-          '700': '#008c91',
-          '800': '#006c72',
-          '900': '#04585d',
-        },      
+        'cyan': '#00FFFF',
+        'yellow': '#FFFF00',
+        'magenta': '#FF00FF',
         'rose': "#FF3053",
-        'dark': '#10102E'
+        'dark': '#10102E',
+        'light': '#FFFFFF'
       },
     },
   },
