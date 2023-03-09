ff15c5736455a12e59c2237f206c2233ea037654
hexcyan <xcyan.li@gmail.com>
2023-03-10 02:10:48 +1100

    typescripting

---
+++ web/src/app.tsx
@@ -0,0 +1,12 @@
+import Card from "./components/card";
+
+function App() {
+    return (
+        <div className="flex shrink-0 h-screen justify-center place-items-center bg-dark">
+            <Card />
+        </div>
+    )
+}
+
+
+export default App
