ae686810421f0517232b29750d913ca590f85f76
hexcyan <101159102+hexcyan@users.noreply.github.com>
2024-03-21 22:48:31 +1100

    initial porting stuff to astro

---
+++ web/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'hexcyan';
+export const SITE_DESCRIPTION = 'welcome to my site :)';
