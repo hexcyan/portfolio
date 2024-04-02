d96e85a0a10186fd6dca9d23c00c8ac472beaff3
hexcyan <101159102+hexcyan@users.noreply.github.com>
2024-04-02 14:45:44 +1100

    set up modern files

--- web/src/consts.ts
+++ web/src/consts.ts
@@ -1,5 +1,25 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// Define color types
+type RGB = `rgb(${number}, ${number}, ${number})`;
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HEX = `#${string}`;
+
+type Color = RGB | RGBA | HEX;
+
+interface Page {
+  name: string;
+  color: Color;
+}
+
+export const SITE_PAGES: Page[] = [
+  {
+    name: "home", 
+    color: '#FFFFFF'
+  },
+  {
+    name: "blog", 
+    color: '#FFFF00'
+  },
+]
 
 export const SITE_TITLE = 'hexcyan';
 export const SITE_DESCRIPTION = 'welcome to my site :)';
