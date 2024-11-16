732b3d7bfca8fe5a22a211bcf4414dabd7f1db4c
hexcyan <101159102+hexcyan@users.noreply.github.com>
2024-11-16 22:19:09 +1100

    fixing Image loading and first blog post

--- src/app/(default)/blog/[slug]/page.tsx
+++ src/app/(default)/blog/[slug]/page.tsx
@@ -3,6 +3,31 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
 import styles from "../../../blog.module.css";
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import { ComponentProps } from "react";
+
+const CustomImage = (props: ComponentProps<"img">) => {
+    const { src, alt } = props;
+    if (!src) return null;
+    console.log(src);
+
+    return (
+        <div className="my-6">
+            <Image
+                src={src}
+                alt={alt || ""}
+                width={800}
+                height={400}
+                className="w-full h-auto"
+                style={{ objectFit: "contain" }}
+            />
+        </div>
+    );
+};
+
+const components = {
+    img: CustomImage,
+};
 
 const options: MDXRemoteProps["options"] = {
     mdxOptions: {
@@ -20,7 +45,11 @@ export default async function Blog({ params }: { params: { slug: string } }) {
         return (
             <article className={styles.prose}>
                 <h1>{post.fm.title}</h1>
-                <MDXRemote source={post.content} options={options} />
+                <MDXRemote
+                    source={post.content}
+                    options={options}
+                    components={components}
+                />
             </article>
         );
     } catch {
