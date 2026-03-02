fe012999c7a01ae3f4f14b51c3937cb05c66cb82
hexcyan <101159102+hexcyan@users.noreply.github.com>
2026-03-02 16:34:27 +1100

    debugging popupwindow for safari

--- src/app/(other)/not-found.tsx
+++ src/app/(other)/not-found.tsx
@@ -3,10 +3,20 @@ import MyButton from "@/components/MyButton";
 
 export default function Home() {
     return (
-        <PopupWindow>
-            <h3>404 - Page Not Found</h3>
-            <p>The page you are looking for does not exist.</p>
-            <MyButton text="go back" path="/" />
-        </PopupWindow>
+        <div
+            style={{
+                position: "fixed",
+                inset: 0,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <PopupWindow>
+                <h3>404 - Page Not Found</h3>
+                <p>The page you are looking for does not exist.</p>
+                <MyButton text="go back" path="/" />
+            </PopupWindow>
+        </div>
     );
 }
